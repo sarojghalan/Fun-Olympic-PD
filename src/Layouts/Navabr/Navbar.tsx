84e6a35729a036d9905bab28119e1c3e2e4baccd
Hypekage <jmestmnag@gmail.com>
2023-06-19 00:03:37 +0545

    navbar section completed

--- src/Layouts/Navabr/Navbar.tsx
+++ src/Layouts/Navabr/Navbar.tsx
@@ -1,14 +1,18 @@
 import SampleButton from "../../Components/Button/SampleButton";
 import { logo } from "../../assets";
+import "../../Scss/Main.scss";
 
 function Navbar() {
+  const LoginHandler = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+  };
   const RegisterHandler = (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
   };
 
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <div className="container-fluid">
+      <div className="container">
         <a className="navbar-brand" href="#">
           <img className="logo" src={logo} alt="logo" />
         </a>
@@ -27,62 +31,40 @@ function Navbar() {
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
               <a className="nav-link active" aria-current="page" href="#">
-                Home
+                Categories
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">
-                Link
+              <a className="nav-link active" aria-current="page" href="#">
+                Favorite
               </a>
-            </li>
-            <li className="nav-item dropdown">
-              <a
-                className="nav-link dropdown-toggle"
-                href="#"
-                id="navbarDropdown"
-                role="button"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
-              >
-                Dropdown
-              </a>
-              <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Action
-                  </a>
-                </li>
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Another action
-                  </a>
-                </li>
-                <li>
-                  <hr className="dropdown-divider" />
-                </li>
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Something else here
-                  </a>
-                </li>
-              </ul>
             </li>
             <li className="nav-item">
-              <a className="nav-link disabled">Disabled</a>
+              <a className="nav-link active" aria-current="page" href="#">
+                live
+              </a>
             </li>
           </ul>
-          <form className="d-flex">
-            <button className="btn btn-outline-success" type="submit">
-              Login
-            </button>
-            <SampleButton
-              title="Register"
-              nameOfClass=""
-              handler={(e: React.MouseEvent<HTMLButtonElement>) =>
-                RegisterHandler(e)
-              }
-            />
-          </form>
+          <div className="nav__right__section">
+            <div>
+              <SampleButton
+                title="Login"
+                nameOfClass=""
+                handler={(e: React.MouseEvent<HTMLButtonElement>) =>
+                  LoginHandler(e)
+                }
+              />
+            </div>
+            <div>
+              <SampleButton
+                title="Register"
+                nameOfClass=""
+                handler={(e: React.MouseEvent<HTMLButtonElement>) =>
+                  RegisterHandler(e)
+                }
+              />
+            </div>
+          </div>
         </div>
       </div>
     </nav>
