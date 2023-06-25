79b3a3b39aaf55eeaaad39c68c8615f6a1f4f14e
Hypekage <jmestmnag@gmail.com>
2023-06-25 08:21:27 +0545

    category page created

--- src/Layouts/Navabr/Navbar.tsx
+++ src/Layouts/Navabr/Navbar.tsx
@@ -34,7 +34,7 @@ function Navbar() {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#">
+              <a className="nav-link active" aria-current="page" href="/category">
                 Categories
               </a>
             </li>
--- src/Pages/Category.tsx
+++ src/Pages/Category.tsx
@@ -1,8 +1,11 @@
 import CategoriesCard from "../Components/Card/CategoriesCard";
+import Title from "../Components/Title/Title";
+import "../Scss/Main.scss";
 
 function Category() {
   return (
     <div className="container">
+      <Title title="CATEGORY" />
       <div className="category__wrapper">
         <div className="category__content__wrapper">
           <CategoriesCard
@@ -11,21 +14,18 @@ function Category() {
           />
         </div>
         <div className="category__content__wrapper">
+          <CategoriesCard title="Football" iconClass="fa-solid fa-futbol" />
+        </div>
+        <div className="category__content__wrapper">
           <CategoriesCard
-            title="Swimming"
-            iconClass="fa-solid fa-person-swimming"
+            title="Racing"
+            iconClass="fa-solid fa-flag-checkered"
           />
         </div>
         <div className="category__content__wrapper">
           <CategoriesCard
-            title="Swimming"
-            iconClass="fa-solid fa-person-swimming"
-          />
-        </div>
-        <div className="category__content__wrapper">
-          <CategoriesCard
-            title="Swimming"
-            iconClass="fa-solid fa-person-swimming"
+            title="Basket Ball"
+            iconClass="fa-solid fa-basketball"
           />
         </div>
       </div>
