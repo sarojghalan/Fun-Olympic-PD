ced7384483d8f983d1deb890d4d4af268ef09633
Hypekage <jmestmnag@gmail.com>
2023-07-04 00:13:13 +0545

    update password and edit info validation completed

--- src/Pages/Profile.tsx
+++ src/Pages/Profile.tsx
@@ -1,20 +1,91 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import SampleButton from "../Components/Button/SampleButton";
 import Title from "../Components/Title/Title";
 import { fun, logo } from "../assets";
 import { ActiveUserContext } from "../context/ActiveUser";
+import { useSnackbar } from "notistack";
 
+interface initialStateI {
+  fullName?: string;
+  age?: string;
+  email?: string;
+  password?: string;
+  confirmPassword?: string;
+  currentPassword?: string;
+}
+
+const initialState: initialStateI = {
+  fullName: "",
+  age: "",
+  email: "",
+};
+const passwordState: initialStateI = {
+  password: "",
+  confirmPassword: "",
+  currentPassword: "",
+};
 function Profile() {
   const { activeUser, setActiveUser } = useContext(ActiveUserContext);
+  const [infoData, setInfoData] = useState<initialStateI>(initialState);
+  const [passwordData, setPasswordData] =
+    useState<initialStateI>(passwordState);
+  const { enqueueSnackbar } = useSnackbar();
+  const [isInfoChanged, setIsInfoChanged] = useState<boolean>(false);
+
   const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
     e.preventDefault();
-    setActiveUser({...activeUser!,})
+    const name = e.target.name;
+    const value = e.target.value;
+    if (
+      name === "password" ||
+      name === "confirmPassword" ||
+      name === "currentPassword"
+    ) {
+      setPasswordData({ ...passwordData, [name]: value });
+    } else {
+      setInfoData({ ...infoData, [name]: value });
+      setIsInfoChanged(true);
+    }
   };
+  useEffect(() => {
+    setInfoData({
+      fullName: activeUser?.fullName,
+      age: activeUser?.age,
+      email: activeUser?.email,
+    });
+  }, []);
   const infoHandler = (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
+    if (isInfoChanged) {
+      setActiveUser({ ...activeUser!, ...infoData });
+      enqueueSnackbar("information has been uploaded successfully", {
+        variant: "success",
+      });
+      setIsInfoChanged(false);
+    } else {
+      enqueueSnackbar("Nothing has changed", {
+        variant: "error",
+      });
+      setIsInfoChanged(false);
+    }
   };
   const passwordHandler = (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
+    if (
+      (passwordData.password === "",
+      passwordData.currentPassword === "" ||
+        passwordData.confirmPassword === "")
+    ) {
+      enqueueSnackbar("Please Insert First. Thank you", {
+        variant: "error",
+      });
+    } else {
+      setActiveUser({ ...activeUser!, ...infoData });
+      enqueueSnackbar("information has been uploaded successfully", {
+        variant: "success",
+      });
+      setIsInfoChanged(false);
+    }
   };
 
   console.log("active user : ", activeUser);
@@ -31,7 +102,7 @@ function Profile() {
                 <input
                   type="text"
                   name="fullName"
-                  value={activeUser?.fullName}
+                  value={infoData?.fullName}
                   placeholder="Full Name ..."
                   onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
                     onChangeHandler(e);
@@ -45,7 +116,7 @@ function Profile() {
                   type="text"
                   placeholder="Age ..."
                   name="age"
-                  value={activeUser?.age}
+                  value={infoData?.age}
                   onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
                     onChangeHandler(e);
                   }}
@@ -58,7 +129,7 @@ function Profile() {
                   type="text"
                   placeholder="Email ..."
                   name="email"
-                  value={activeUser?.email}
+                  value={infoData?.email}
                   onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
                     onChangeHandler(e);
                   }}
@@ -83,17 +154,41 @@ function Profile() {
             <div className="account__information">
               <div className="form__input">
                 <label htmlFor="">Current Password : </label> <br />
-                <input type="text" placeholder="Full Name ..." />
+                <input
+                  type="text"
+                  placeholder="Full Name ..."
+                  name="currentPassword"
+                  value={passwordData?.currentPassword}
+                  onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                    onChangeHandler(e);
+                  }}
+                />
               </div>
               <div className="form__input">
                 <label htmlFor="">New Password : </label>
                 <br />
-                <input type="text" placeholder="Age ..." />
+                <input
+                  type="text"
+                  placeholder="Age ..."
+                  name="password"
+                  value={passwordData?.password}
+                  onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                    onChangeHandler(e);
+                  }}
+                />
               </div>
               <div className="form__input">
                 <label htmlFor="">Confirm New Password : </label>
                 <br />
-                <input type="text" placeholder="Age ..." />
+                <input
+                  type="text"
+                  placeholder="Age ..."
+                  name="confirmPassword"
+                  value={passwordData?.confirmPassword}
+                  onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                    onChangeHandler(e);
+                  }}
+                />
               </div>
               <SampleButton
                 nameOfClass="mb-4"
